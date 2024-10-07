6ccdf7f7b2ac26bfc86c3f203d4900ffbc1ddb3d
learncodewithsaurabh <saurabhkumar93289@gmail.com>
2024-10-07 16:41:37 +0530

    updated 39, 40, and 41.

--- src/Java8Programs/FirstNonRepeatedCharacter_41.java
+++ src/Java8Programs/FirstNonRepeatedCharacter_41.java
@@ -4,12 +4,12 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /*Q41. Write a program to find first non-repeated character in a string using the Java 8 Streams API.
-    Sample;	Input:  input = "launcher";
-            Output: l
+    Sample;	Input:  input = "Saurabh";
+            Output: S
 */
 public class FirstNonRepeatedCharacter_41 {
     public static void main(String[] args) {
-        String input = "launcher";
+        String input = "Saurabh";
         Map<Character,Integer> characterCount = new LinkedHashMap<>();
         for(char c : input.toCharArray()){
             characterCount.put(c,characterCount.getOrDefault(c,0)+1);
